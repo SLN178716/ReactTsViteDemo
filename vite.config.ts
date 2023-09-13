ef046a2775d97d73011c2470f71a97a435785673
lnshi <lnshi@jinhetech.com>
2023-09-13 16:33:44 +0800

    规范化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
+  // 配置路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  // 代理
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://yourBaseUrl',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+        secure: false,
+      },
+    },
+  },
 })
